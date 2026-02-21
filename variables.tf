12c4e2236a5440c8f3893727c765f83f1b7ec74a
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2026-02-21 10:10:40 +0530

    modified:   README.md

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "project_name" {
+  type        = string
+  description = "Project Name"
+  default     = "rke2"
+}
+
+variable "sg_ports" {
+  type        = list(number)
+  description = "Security Group Ports"
+  default     = [22, 80, 443, 2379, 6443, 10250]
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance Type"
+  default     = "t3a.medium"
+}
+
+variable "volume_size" {
+  type        = number
+  description = "Volume Size"
+  default     = 100
+}
+
+variable "volume_type" {
+  type    = string
+  default = "gp3"
+}
+
+variable "spot_instance" {
+  type    = bool
+  default = false
+}
+
+variable "spot_type" {
+  type    = string
+  default = "one-time"
+}
